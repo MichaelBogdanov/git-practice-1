66fbff4a345a923874d83e10368fc269e757b0b2
Radio <aleksvasilev269@gmail.com>
2026-02-07 01:31:27 +0300

    Work is completed: task3, task6, task9 done

--- 2992/vasilevA_ryahov/task3/solution.py
+++ 2992/vasilevA_ryahov/task3/solution.py
@@ -1,16 +1,27 @@
 import sys
 
+def rotate_words(file: str):
+    """Перевернуть порядок слов в тексте (и строк)"""
+    with open(file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    result = []
+    
+    for line in reversed(lines):
+        line = line.strip()
+        
+        if line:
+            words = line.split()
+            reversed_words = ' '.join(words[::-1])
+            result.append(reversed_words)
+        else:
+            result.append('')
+    
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(result))
+          
 if len(sys.argv) > 1:
     file = sys.argv[1]
+    rotate_words(file)
 else:
     raise Exception('Укажите имя файла: python solution.py file.txt')
-
-with open(file, 'r', encoding='utf-8') as f:
-    data = f.readlines()
-
-result = []
-for s in data:
-    result.append(' '.join(s.split()[::-1]))
-
-with open(file, 'w', encoding='utf-8') as f:
-    f.write('\n'.join(result))
