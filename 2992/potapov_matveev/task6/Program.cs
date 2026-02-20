f9d8b882e7c344d126bf4fd5cdc5db6d7dbe03c9
HeKuToCuK <potapov1269@gmail.com>
2026-02-21 01:22:42 +0300

    Выполнение первого задания (номер 6)

---
+++ 2992/potapov_matveev/task6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    public class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 64, 34, 25, 12, 22, 11, 90 };
+
+            Console.WriteLine("Исходный массив: " + string.Join(", ", numbers));
+
+            BubbleSort.Sort(numbers);
+
+            Console.WriteLine("Отсортированный массив: " + string.Join(", ", numbers));
+        }
+    }
+}
