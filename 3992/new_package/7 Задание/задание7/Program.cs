98fab4bb5303cfd8a54d51bb3ae99d76200689a5
sashin_and_isupov <sashin_and_isupov@gmail.com>
2026-03-06 14:11:41 +0300

    Консольное приложение на C#, которое анализирует текстовый файл и подсчитывает частоту каждого символа

---
+++ 3992/new_package/7 Задание/задание7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Анализ частоты символов в файле");
+        Console.WriteLine("================================");
+        
+        Console.Write("Введите путь к файлу: ");
+        string filePath = Console.ReadLine();
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("Файл не найден!");
+            return;
+        }
+
+        try
+        {
+            string text = File.ReadAllText(filePath);
+
+            Dictionary<char, int> frequencies = new Dictionary<char, int>();
+
+            foreach (char c in text)
+            {
+                if (frequencies.ContainsKey(c))
+                    frequencies[c]++;
+                else
+                    frequencies[c] = 1;
+            }
+
+            Console.WriteLine($"\nВсего символов: {text.Length}");
+            Console.WriteLine("Результаты:");
+
+            foreach (var item in frequencies)
+            {
+                if (item.Key != ' ' && item.Key != '\n' && item.Key != '\r' && item.Key != '\t')
+                {
+                    Console.WriteLine($"'{item.Key}': {item.Value} раз");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
