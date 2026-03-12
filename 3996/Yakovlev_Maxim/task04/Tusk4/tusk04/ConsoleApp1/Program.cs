01129e3cc12ee24eab4c97310e8bd18283af3c7b
Minimaxind <Maksim4877@inbox.ru>
2026-03-12 21:05:50 +0300

    Задание

---
+++ 3996/Yakovlev_Maxim/task04/Tusk4/tusk04/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿string text = File.ReadAllText("file.txt");
+string[] elements = text.Split(' ');
+
+List<string> unique = new List<string>();
+
+for (int i = 0; i < elements.Length; i++)
+{
+    if (!unique.Contains(elements[i]))
+    {
+        unique.Add(elements[i]);
+    }
+}
+
+File.WriteAllText("output.txt", string.Join(" ", unique));
