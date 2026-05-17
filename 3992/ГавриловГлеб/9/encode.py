ea501a238b81de7ca3982cd27726e1f80310186e
GLEB <gleb.gavrilov.17@bk.ru>
2026-05-17 17:04:50 +0300

    pr1 + ".idea"

---
+++ 3992/ГавриловГлеб/9/encode.py
@@ -0,0 +1,15 @@
+try:
+    s = input().strip()
+    r = []
+    c = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            c += 1
+        else:
+            r.append(s[i-1] + str(c))
+            c = 1
+    if s:
+        r.append(s[-1] + str(c))
+    print("".join(r))
+except Exception:
+    print("Ошибочка")
